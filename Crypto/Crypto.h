3ba20f267601b9cba3ea43a02d68c405d71402d2
Samira.Daneshyar <Samira.Daneshyar@Samira.local>
2016-11-02 15:25:31 -0400

    initial commit

---
+++ Crypto/Crypto.h
@@ -0,0 +1,19 @@
+//
+//  Crypto.h
+//  Crypto
+//
+//  Created by Samira.Daneshyar on 2016-11-02.
+//  Copyright © 2016 CGI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Crypto.
+FOUNDATION_EXPORT double CryptoVersionNumber;
+
+//! Project version string for Crypto.
+FOUNDATION_EXPORT const unsigned char CryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Crypto/PublicHeader.h>
+
+
---
+++ Crypto/CryptoSample-Bridge.h
@@ -0,0 +1,10 @@
+//
+//  CryptoSample-Bridge.h
+//  CryptoSample
+//
+//  Created by Samira Daneshyar on 10/30/16.
+//  Copyright © 2016 Samira Daneshyar. All rights reserved.
+//
+
+#import <CommonCrypto/CommonCrypto.h>
+
